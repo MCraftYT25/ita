52f82182867a88d2d18dd337373966de664adba0
MCraftYT25 <93701162+MCraftYT25@users.noreply.github.com>
2023-12-20 11:26:50 +0100

    Add files via upload
    V0.6

---
+++ scripts/move.js
@@ -0,0 +1,22 @@
+function move(x)
+    {
+        switch(x)
+        {
+            case 'ibb':
+                window.location.href = "https://mcraftyt25.github.io/ibb/";
+                break;
+            case 'ling':
+                window.location.href = "https://mcraftyt25.github.io/ling/";
+                break;
+            case 'archive':
+                window.location.href = "https://mcraftyt25.github.io/angwebsite.github.io/";
+                break;
+            case '48':
+                window.location.href = "https://mcraftyt25.github.io/48/";
+                break;
+            case '':
+                window.location.href = "https://mcraftyt25.github.io/angwebsite.github.io/";
+                break;
+                
+        }
+    }
